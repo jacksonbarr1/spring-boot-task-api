2d75d3de818cde626447927c4a1c76e3626ed1c0
Jackson Barr <jacksonbarr2021@gmail.com>
2023-05-23 21:47:39 -0400

    Create Task entity

---
+++ src/main/java/com/example/taskmaster/entity/Task.java
@@ -0,0 +1,39 @@
+package com.example.taskmaster.entity;
+
+import com.example.taskmaster.dto.Priority;
+import com.example.taskmaster.dto.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private TaskmasterUser user;
+
+
+    private String taskName;
+    private String description;
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+    private LocalDate createdDate;
+    private LocalDate lastUpdatedDate;
+
+}
