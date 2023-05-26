09c1b2bebb497514434293e26cb171312fba5745
Jackson Barr <jbarr39@gatech.edu>
2023-05-26 03:40:12 -0400

    Remove unused imports

--- src/main/java/com/example/taskmaster/controller/AuthController.java
+++ src/main/java/com/example/taskmaster/controller/AuthController.java
@@ -3,20 +3,14 @@ package com.example.taskmaster.controller;
 
 import com.example.taskmaster.dto.AuthRequest;
 import com.example.taskmaster.dto.UserDTO;
-import com.example.taskmaster.entity.TaskmasterUser;
 import com.example.taskmaster.service.AuthService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
